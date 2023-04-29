fcc96b23aa1b60a4498cdda2507a09bef2dfb892
chillyarok <122738094+chillyarok@users.noreply.github.com>
2023-04-29 09:30:44 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,10 @@
+font = 3
+button_width = 10
+button_height = 2
+button_pady = 2.5
+color = "white"
+bgcolor = "gainsboro"
+display_data = ""
+ans = ""
+openai_token = "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"
+engine = "text-davinci-003"
